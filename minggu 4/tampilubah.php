1ec2ed25023695e894c192f6553b6134971e1881
aldo setiawan <83720535+queen-23@users.noreply.github.com>
2021-11-13 11:28:45 +0700

    Create tampilubah.php

---
+++ minggu 4/tampilubah.php
@@ -0,0 +1,48 @@
+<div id="ubah">
+    <form method="post" id="form_mahasiswa">
+        <table>
+            <tr>
+                <td>NIM: </td>
+                <td><input type="number" name="nim"></td>
+            </tr>
+            <tr>
+                <td>Nama: </td>
+                <td><input type="text" name="nama"></td>
+            </tr>
+            <tr>
+                <td>Prodi: </td>
+                <td>
+                    <select name="prodi">
+                        <option value="IF">Teknik Informatika</option>
+                        <option value="EL">Teknik Elektro</option>
+                        <option value="SI">Teknik Sipil</option>
+                        <option value="TG">Teknik Geofisika</option>
+                        <option value="MA">Matematika</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td>Angkatan: </td>
+                <td><input type="number" name="angkatan"></td>
+            </tr>
+        </table>
+    </form>
+</div>
+<button id="btn_ubah">Ubah Data</button>
+<script>
+    $(document).ready(function(){
+        $('#btn_ubah').click(function(){
+            var data = $('#form_mahasiswa').serialize();
+            $.ajax({
+                type    : 'POST',
+                url     : "ubah.php",
+                data    : data,
+                cache   : false,
+                success : function(data){
+                    $('#formulir').load("tampilubah.php")
+                    $('#tampil_data').load("tampil.php");
+                }
+            });
+        });
+    });
+</script>
